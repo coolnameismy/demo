8442adf7456850d18531737592febe8ba614c03e
liuyanwei <liu.yanwei29@zte.com.cn>
2015-08-14 15:34:29 +0800

    上传bledemo

---
+++ BleDemo/BleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BleDemo
+//
+//  Created by ZTELiuyw on 15/8/13.
+//  Copyright (c) 2015年 liuyanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BleDemo/BleDemo/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  BleDemo
+//
+//  Created by ZTELiuyw on 15/8/13.
+//  Copyright (c) 2015年 liuyanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+
+@interface ViewController : UIViewController<CBCentralManagerDelegate,CBPeripheralDelegate>
+
+
+
+@end
+
