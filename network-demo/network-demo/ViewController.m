e4688be87dc3b306f4b77df6981be9dbbb93e87a
coolnameismy <coolnameismy@hotmail.com>
2016-01-30 23:08:59 +0800

    m

--- network-demo/network-demo/ViewController.m
+++ network-demo/network-demo/ViewController.m
@@ -16,32 +16,96 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
     //string 转 url编码
-    NSString *urlString = @"http://wwW.xxx.com?query= this is question";
-    NSLog(@"%@",urlString);
-
-//    NSURL *url = [NSURL URLWithString:urlString];
-//    NSLog(@"%@",url.query);
-
+//    NSString *urlString = @"http://www.yczhsq.com/overview/qryContents";
+    NSString *urlString = @"https://api.github.com/users/coolnameismy";
     NSURL *url = [NSURL URLWithString:[urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
-    NSLog(@"%@",url.query);
-    
-    
-    
-//    NSLog(@"%@",[urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]);
-//          
-//    NSLog(@"%@",[urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]);
-//    NSLog(@"---------%@--------",urlString);
-//    NSString *urlEncodeString = [urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet uppercaseLetterCharacterSet]];
-//   NSLog(@"%@",urlEncodeString);
-//    urlEncodeString = [urlEncodeString stringByRemovingPercentEncoding];
-////        [urlEncodeString stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-//   NSLog(@"%@",urlEncodeString);
-//
 
     
+    NSURLRequest *request = [[NSURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
+//    NSURLResponse *response;
+    NSURLConnection *connection = [[NSURLConnection alloc]initWithRequest:request delegate:self];
+    
+    [connection start];
+}
+
+//请求失败
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    NSLog(@"=================didFailWithError=================");
+    NSLog(@"error:%@",error);
+}
+
+//重定向
+- (nullable NSURLRequest *)connection:(NSURLConnection *)connection willSendRequest:(NSURLRequest *)request redirectResponse:(nullable NSURLResponse *)response{
+    NSLog(@"=================request redirectResponse=================");
+    NSLog(@"request:%@",request);
+    return request;
+}
+
+//接收响应
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+    NSLog(@"=================didReceiveResponse=================");
+    NSHTTPURLResponse *resp = (NSHTTPURLResponse *)response;
+    NSLog(@"response:%@",resp);
+}
+
+//接收响应
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    NSLog(@"=================didReceiveData=================");
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+    NSLog(@"data:%@",dic);
+}
+
+//- (nullable NSInputStream *)connection:(NSURLConnection *)connection needNewBodyStream:(NSURLRequest *)request{
+//    
+//}
+
+//上传数据委托，用于显示上传进度
+- (void)connection:(NSURLConnection *)connection   didSendBodyData:(NSInteger)bytesWritten
+ totalBytesWritten:(NSInteger)totalBytesWritten
+totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite{
+    NSLog(@"=================totalBytesWritten=================");
+}
+
+//- (nullable NSCachedURLResponse *)connection:(NSURLConnection *)connection willCacheResponse:(NSCachedURLResponse *)cachedResponse{
+//    
+//}
+
+//完成请求
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    NSLog(@"=================connectionDidFinishLoading=================");
+}
+
+
+#pragma mark -https认证
+-(BOOL)connectionShouldUseCredentialStorage:(NSURLConnection *)connection{
+    return true;
+}
+- (void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge{
+    NSLog(@"didReceiveAuthenticationChallenge %@ %zd", [[challenge protectionSpace] authenticationMethod], (ssize_t) [challenge previousFailureCount]);
+    // 1.从服务器返回的受保护空间中拿到证书的类型
+    // 2.判断服务器返回的证书是否是服务器信任的
+    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]){
+
+        NSLog(@"是服务器信任的证书");
+        // 3.根据服务器返回的受保护空间创建一个证书
+        //         void (^)(NSURLSessionAuthChallengeDisposition, NSURLCredential *)
+        //         代理方法的completionHandler block接收两个参数:
+        //         第一个参数: 代表如何处理证书
+        //         第二个参数: 代表需要处理哪个证书
+        //创建证书
+        NSURLCredential *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+        // 4.安装证书   completionHandler(NSURLSessionAuthChallengeUseCredential , credential);
+        
+        
+        [[challenge sender]  useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
+        [[challenge sender]  continueWithoutCredentialForAuthenticationChallenge: challenge];
+    }
+}
+
+- (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace{
+       return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
 }
 
 - (void)didReceiveMemoryWarning {
