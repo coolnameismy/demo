daa1c0390209766f3cfd9b5866b64f89544fcdb4
coolnameismy <coolnameismy@hotmail.com>
2016-03-07 23:03:09 +0800

    m

---
+++ foo/LoveFreshBeenCopy/LoveFreshBeenCopy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveFreshBeenCopy
+//
+//  Created by 刘彦玮 on 16/3/7.
+//  Copyright © 2016年 刘彦玮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
